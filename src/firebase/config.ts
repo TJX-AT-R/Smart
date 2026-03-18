0b0c628737bd55fc5cbeeeeb64d68475e9980266
Thubelihle Ncube <ncubethubelihle483@gmail.com>
2026-03-18 18:45:58 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-957768860-60c85",
+  "appId": "1:846408412261:web:67ae586620a88bfeb5aa8c",
+  "apiKey": "AIzaSyDXE1TZNL8Fkdupqjt2sNkjLMAtBWffzrM",
+  "authDomain": "studio-957768860-60c85.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "846408412261"
+};
