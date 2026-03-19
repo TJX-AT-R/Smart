e72c737882bb1c5196b625ed9f54f8d643e4c9d3
Thubelihle Ncube <ncubethubelihle483@gmail.com>
2026-03-19 06:39:41 +0000

    Fix the storage bucket property for the website

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:846408412261:web:67ae586620a88bfeb5aa8c",
   "apiKey": "AIzaSyDXE1TZNL8Fkdupqjt2sNkjLMAtBWffzrM",
   "authDomain": "studio-957768860-60c85.firebaseapp.com",
+  "storageBucket": "studio-957768860-60c85.firebasestorage.app",
   "measurementId": "",
   "messagingSenderId": "846408412261"
 };
